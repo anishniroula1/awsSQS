8665347daf75ab8bc64c6d10a570ad4c1254d6bd
anishniroula <anishniroula1@gmail.com>
2023-11-03 10:49:03 -0400

    delete

--- tests/test_interface_aws.py
+++ tests/test_interface_aws.py
@@ -17,3 +17,21 @@ def test_get_object():
     body = result['Body'].read().decode('utf-8')
 
     assert body == 'Testing', f'Expected "Testing" but got "{body}"'
+
+@mock_s3
+def test_delete_s3_object():
+    # Set up the mocked S3 environment
+    conn = boto3.resource('s3', region_name='us-east-1')
+    bucket_name = 'test-bucket'
+    s3_key = 'test-key'
+    conn.create_bucket(Bucket=bucket_name)
+    conn.Object(bucket_name, s3_key).put(Body='Testing')
+
+    # Create an instance of InterfaceAWS and call delete_s3_object
+    interface_aws = InterfaceAWS()
+    result = interface_aws.delete_s3_object(s3_key, bucket_name)
+
+    # Check if the object has been deleted
+    bucket = conn.Bucket(bucket_name)
+    objects = list(bucket.objects.filter(Prefix=s3_key))
+    assert len(objects) == 0, f'Expected 0 objects but found {len(objects)}'
