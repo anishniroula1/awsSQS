e8dbd31bb1d5c6944e063830d6e19a1dd281c0c2
anishniroula <anishniroula1@gmail.com>
2023-11-03 08:09:32 -0400

    class tutorial

---
+++ class_example.py
@@ -0,0 +1,19 @@
+class MyClass:
+    def __init__(self, value: str = None):
+        self.value = value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+my_object = MyClass()
+def modify_object(new_value):
+    x = {'value': new_value}
+    my_object = MyClass(x)
+    print(my_object.get_value())
+
+modify_object("Hello, World!")
+
+# print(my_object.get_value())
---
+++ userExample.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Optional
+import dacite
+import json
+
+@dataclass
+class Person:
+    count: Optional[int]
+
+@dataclass
+class Age:
+    number: int
+
+@dataclass
+class User(Age, Person):
+    name: str
+
+    def multiply_age(self):
+        return self.number * 20
+
+json_data = {"name": "John", "age": {"number": 25}, 'number': 100, 'count': 300}
+# data_dict = json.loads(json_data)
+user = dacite.from_dict(User, json_data)
+print(user.multiply_age())
