a835927dd30a235ed91e3c4d5b553bdf847638ad
anishniroula <anishniroula1@gmail.com>
2024-01-30 18:16:03 -0500

    test

--- CutomLogger.py
+++ CutomLogger.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 import os
 
@@ -22,39 +23,20 @@ class CustomLogger(logging.Logger):
         Find the stack frame of the caller so that we can note the source
         file name, line number and function name.
         """
-        f = logging.currentframe()
-        # On some versions of IronPython, currentframe() returns None if
-        # IronPython isn't run with -X:Frames.
-        if f is not None:
-            f = f.f_back
-        orig_f = f
-        while f and stacklevel > 1:
-            f = f.f_back
-            stacklevel -= 1
-        if not f:
-            f = orig_f
-        rv = "(unknown file)", 0, "(unknown function)", None
-        while hasattr(f, "f_code"):
-            co = f.f_code
-            filename = os.path.normcase(co.co_filename)
-            if filename == logging._srcfile:
-                f = f.f_back
-                continue
-            rv = (co.co_filename, f.f_lineno, co.co_name, None)
-            break
-        return rv
+        frame = inspect.currentframe()
+        while frame:
+            frame_info = inspect.getframeinfo(frame)
+            filename = frame_info.filename
+            # Check if the frame is outside of the logging module
+            if "logging" not in filename and filename != __file__:
+                return (filename, frame_info.lineno, frame_info.function, None)
+            frame = frame.f_back
+        return "(unknown file)", 0, "(unknown function)", None
 
     def _log_with_default_message(self, level, msg, args, kwargs, show_default):
         fn, lno, func, sinfo = self.find_caller()
-        # if show_default:
-        #     msg = f"{self.DEFAULT_MESSAGE}{msg}"
         record = self.makeRecord(self.name, level, fn, lno, msg, args, kwargs, func)
         self.handle(record)
-
-    def _log(self, level, msg, args, **kwargs):
-        # extra = kwargs.pop('extra', {})
-        # extra['show_default'] = kwargs.pop('show_default', False)
-        super()._log(level, msg, args, kwargs)
 
     def debug(self, msg, *args, show_default=False, **kwargs):
         self._log_with_default_message(logging.DEBUG, msg, args, kwargs, show_default)
