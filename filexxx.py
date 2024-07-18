a5c8e58bfa32dd17bdaa67aefb01277376b3d496
anishniroula <anishniroula1@gmail.com>
2024-07-18 11:41:51 -0400

    tets

--- filexxx.py
+++ filexxx.py
@@ -1,36 +1,45 @@
-import datetime
+from datetime import datetime
 
 # Sample data
-files = [
-    {"fileName": "file1.txt", "createdDate": "2024-03-25"},
-    {"fileName": "file2.docx", "createdDate": "2024-05-15"},
-    {"fileName": "file3.pdf", "createdDate": "2024-06-01"},
-    {"fileName": "file4.txt", "createdDate": "2024-02-20"},
+records = [
+    {"fileName": "document1.docx", "createdDate": "2024-03-27T12:42:15.072+00:00"},
+    {"fileName": "document2.pdf", "createdDate": "2024-05-01T12:42:15.072+00:00"},
+    {"fileName": "document3.txt", "createdDate": "2024-02-15T12:42:15.072+00:00"},
+    {"fileName": "document4.docx", "createdDate": "2024-04-10T12:42:15.072+00:00"}
 ]
 
-# Function to filter the list
-def filter_files(files):
-    filtered_files = []
-    error_log = []
+# Function to filter records
+def filter_records(records):
+    filtered_records = []
+    error_logs = []
 
-    for file in files:
-        if not file['fileName'].endswith('.docx'):
-            created_date = datetime.datetime.strptime(file['createdDate'], "%Y-%m-%d")
-            if created_date > datetime.datetime(2024, 4, 30):
-                error_log.append(f"Error: {file['fileName']} has a createdDate later than April 2024")
-            else:
-                filtered_files.append(file)
-    
-    return filtered_files, error_log
+    for record in records:
+        file_name = record["fileName"]
+        created_date = record["createdDate"]
 
-# Filter the files
-filtered_files, error_log = filter_files(files)
+        # Check if the file name does not end with .docx
+        if not file_name.endswith(".docx"):
+            filtered_records.append(record)
 
-# Output the results
-print("Filtered Files:")
-for file in filtered_files:
-    print(file)
+        # Parse the created date and check if it is later than April 2024
+        date_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+        created_date_parsed = datetime.strptime(created_date, date_format)
+        april_2024 = datetime(2024, 4, 30, 23, 59, 59, tzinfo=created_date_parsed.tzinfo)
 
-print("\nError Log:")
-for error in error_log:
+        if created_date_parsed > april_2024:
+            error_logs.append(f"Error: {file_name} has a created date later than April 2024.")
+
+    return filtered_records, error_logs
+
+# Filter the records
+filtered_records, error_logs = filter_records(records)
+
+# Print filtered records
+print("Filtered Records:")
+for record in filtered_records:
+    print(record)
+
+# Print error logs
+print("\nError Logs:")
+for error in error_logs:
     print(error)
