010499578c074c6efd0846c927cca942dc1b0d72
anishniroula <anishniroula1@gmail.com>
2025-03-27 13:16:22 -0400

    test

--- sql_test.py
+++ sql_test.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-# Replace with your actual DB URL
+# Connect to your DB
 engine = create_engine("postgresql://user:password@localhost:5432/your_db_name")
 
 old_id = 123456
@@ -8,22 +8,20 @@ new_id = 789999
 
 update_sql = """
 UPDATE your_table
-SET matches = (
-    SELECT jsonb_agg(
-        CASE
-            WHEN (elem->>'global_id')::int = :old_id
-            THEN jsonb_set(elem, '{global_id}', to_jsonb(:new_id::int))
-            ELSE elem
-        END
-    )
-    FROM jsonb_array_elements(matches) AS elem
+SET matches = jsonb_agg(
+    CASE
+        WHEN (elem->>'global_id')::int = :old_id
+        THEN jsonb_set(elem, '{global_id}', to_jsonb(CAST(:new_id AS int)))
+        ELSE elem
+    END
 )
-WHERE EXISTS (
-    SELECT 1
-    FROM jsonb_array_elements(matches) AS elem
-    WHERE (elem->>'global_id')::int = :old_id
-);
+FROM (
+    SELECT id, jsonb_array_elements(matches) AS elem
+    FROM your_table
+) AS expanded
+WHERE your_table.id = expanded.id
+  AND (elem->>'global_id')::int = :old_id;
 """
 
 with engine.begin() as conn:
-    conn.execute(text(update_sql), {"old_id": old_id, "new_id": new_id})
+    result = conn.execute(text(update_sql), {"old_id": old_id, "new_id": new_id})
