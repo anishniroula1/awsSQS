32bb8515f1b401fab882a1dd2f5f5d52b86e03a0
anishniroula <anishniroula1@gmail.com>
2025-03-27 13:03:00 -0400

    test

--- sql_test.py
+++ sql_test.py
@@ -1,34 +1,29 @@
-from sqlalchemy import text
+from sqlalchemy import create_engine, text
 
-sql_script = text("""
-WITH updated AS (
-    SELECT
-        id, -- replace with your actual primary key column
-        jsonb_agg(
-            CASE
-                WHEN (matcher ->> 'global_id')::int = :old_global_id THEN
-                    jsonb_set(matcher, '{global_id}', to_jsonb(:new_global_id))
-                ELSE matcher
-            END
-        ) AS new_matchers
-    FROM x.table, jsonb_array_elements(matchers) AS matcher
-    GROUP BY id -- replace with your actual primary key column
+# Replace with your actual DB URL
+engine = create_engine("postgresql://user:password@localhost:5432/your_db_name")
+
+old_id = 123456
+new_id = 789999
+
+update_sql = """
+UPDATE your_table
+SET matches = (
+    SELECT jsonb_agg(
+        CASE
+            WHEN (elem->>'global_id')::int = :old_id
+            THEN jsonb_set(elem, '{global_id}', to_jsonb(:new_id::int))
+            ELSE elem
+        END
+    )
+    FROM jsonb_array_elements(matches) AS elem
 )
-UPDATE x.table AS orig
-SET matchers = updated.new_matchers
-FROM updated
-WHERE orig.id = updated.id
-AND orig.matchers @> :json_query
-""")
+WHERE EXISTS (
+    SELECT 1
+    FROM jsonb_array_elements(matches) AS elem
+    WHERE (elem->>'global_id')::int = :old_id
+);
+"""
 
-# Parameters
-old_global_id = 123
-new_global_id = 456
-json_query = f'[{{"global_id": {old_global_id}}}]'
-
-session.execute(sql_script, {
-    'old_global_id': old_global_id,
-    'new_global_id': new_global_id,
-    'json_query': json_query
-})
-session.commit()
+with engine.begin() as conn:
+    conn.execute(text(update_sql), {"old_id": old_id, "new_id": new_id})
